c41bd2092726d85558a75ccb6cad915672d790af
ganeshwadule <uniquegbw09@gmail.com>
2025-02-20 17:49:18 +0530

    templates + scope resolution operatoe(::)

--- templates.cpp
+++ templates.cpp
@@ -4,36 +4,56 @@ using namespace std;
 
 #define INT_MAX 2147483647
 
+template <class T>
+class calculator
+{
+private:
+    T a;
+    T b;
+
+public:
+    calculator(T a, T b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    T add();
+    T sub();
+    T mul();
+    T div();
+
+};  
 template<class T>
-class calculator{
-    private:
-        T a;
-        T b;
-    public:
-        calculator(T a, T b){
-            this->a = a;
-            this->b = b;
-        }
-        T add(){
-            return this->a + this->b;
-        }
+T calculator<T>::add()
+{
+    return this->a + this->b;
+}
 
-        T sub(){
-            return this->a - this->b;
-        }
+template<class T>
+T calculator<T>::sub()
+{
+    return this->a - this->b;
+}
 
-        T mul(){
-            return this->a * this->b;
-        }
+template<class T>
+T calculator<T>::mul()
+{
+    return this->a * this->b;
+}
 
-        T div(){
-            if(this->b == 0)return INT_MAX;
+template<class T>
+T calculator<T>::div()
+{
+    if (this->b == 0)
+        return INT_MAX;
 
-            return this->a / this->b;
-        }
-};
+    return this->a / this->b;
+}
 
-int main(){
-    calculator<int> c(10.1233,12);
-    cout<<c.add();
+int main()
+{
+    calculator<int> c(10.1233, 12);
+    cout << c.add()<<endl;
+    cout<<c.mul();
 }
