75532d176cc6f076bdea7e5a767140b79cee20d7
ganeshwadule <uniquegbw09@gmail.com>
2025-02-20 16:05:11 +0530

    oop

---
+++ oop.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class Rectangle{
+    private:
+        int length;
+        int breadth;
+
+    public:
+        Rectangle(int l,int b){
+            length = l;
+            breadth = b;
+        }
+
+        int area(){
+            return length*breadth;
+        }
+
+        int perimeter(){
+            return 2*(length+breadth);
+        }
+};
+
+
+int main(){
+    
+    int l,b;
+    cin>>l>>b;
+
+    Rectangle r(l,b);
+
+    cout<<r.area()<<" "<<r.perimeter();
+
+}
